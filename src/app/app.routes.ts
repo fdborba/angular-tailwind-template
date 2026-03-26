645858c6f011b595665e760cbc127bae4d11b508
FELIPE DOMANICO BORBA <felipe.borba@tjsp.jus.br>
2026-03-26 18:59:38 -0300

    página MPS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,6 +18,7 @@ import { VideosComponent } from './pages/ui-elements/videos/videos.component';
 import { SignInComponent } from './pages/auth-pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/auth-pages/sign-up/sign-up.component';
 import { CalenderComponent } from './pages/calender/calender.component';
+import { MpsComponent } from './pages/mps/mps.component';
 
 export const routes: Routes = [
   {
@@ -55,6 +56,11 @@ export const routes: Routes = [
         path:'blank',
         component:BlankComponent,
         title:'Angular Blank Dashboard | TailAdmin - Angular Admin Dashboard Template'
+      },
+      {
+        path:'mps',
+        component:MpsComponent,
+        title:'Angular MPS'
       },
       // support tickets
       {
---
+++ src/app/pages/mps/mps.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { PageBreadcrumbComponent } from '../../shared/components/common/page-breadcrumb/page-breadcrumb.component';
+import { LabelComponent } from '../../shared/components/form/label/label.component';
+import { InputFieldComponent } from '../../shared/components/form/input/input-field.component';
+
+@Component({
+  selector: 'app-mps',
+  imports: [
+    PageBreadcrumbComponent,
+    LabelComponent,
+    InputFieldComponent,
+  ],
+  templateUrl: './mps.component.html',
+})
+export class MpsComponent {
+
+}
