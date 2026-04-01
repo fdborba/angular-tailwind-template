f3c16d49e53e4d42b485692aa1308516ed7ad941
FELIPE DOMANICO BORBA <felipe.borba@tjsp.jus.br>
2026-04-01 16:48:17 -0300

    Accordion Card

--- src/app/pages/mps/mps.component.ts
+++ src/app/pages/mps/mps.component.ts
@@ -14,4 +14,25 @@ import { InputFieldComponent } from '../../shared/components/form/input/input-fi
 })
 export class MpsComponent {
 
+toggleAccordion(index: number) {
+    const content = document.getElementById(`content-${index}`);
+    const icon = document.getElementById(`icon-${index}`);
+ 
+    // SVG for Down icon
+    const downSVG = `<i class="fas fa-chevron-down w-4 h-4"></i>`;
+ 
+    // SVG for Up icon
+    const upSVG = `<i class="fas fa-chevron-up w-4 h-4"></i>`;
+ 
+    if (!content || !icon) return;
+    // Toggle the content's max-height for smooth opening and closing
+    if (content.style.maxHeight && content.style.minHeight !== '0px') {
+      content.style.maxHeight = '0';
+      icon.innerHTML = upSVG;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+      icon.innerHTML = downSVG;
+    }
+  }
+
 }
